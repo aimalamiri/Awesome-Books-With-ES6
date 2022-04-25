73b183ab139c5999804ee49817ba9d8a4011c6aa
Aimal Amiri <meaimal@hotmail.com>
2022-04-25 20:22:36 +0430

    Add padding to books list

--- modules/utilities.js
+++ modules/utilities.js
@@ -1,6 +1,6 @@
 export const insertBookIntoDom = (book, booksList) => {
   const bookElement = `
-      <li class="book">
+      <li class="book px-1 py-1">
         <div><strong>"${book.title}"</strong> by ${book.author}</div>
         <button data-id="${book.id}" class="book-remove">Remove</button>
       </li>`;
